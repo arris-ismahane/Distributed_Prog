079873c9997fd071afe1a4abd768259ac83f0c46
arris-ismahane <arrisismahane@gmail.com>
2025-03-28 00:57:25 +0100

    database with minikube

---
+++ jewlery/src/main/java/com/progdist/jewlery/configurations/JpaConfig.java
@@ -0,0 +1,12 @@
+package com.progdist.jewlery.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.progdist.jewlery.repositories")
+public class JpaConfig {
+
+}
