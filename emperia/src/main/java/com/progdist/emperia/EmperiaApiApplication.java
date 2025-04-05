36bf47c3b97b12112b704c4107b0402cabd4ac93
arris-ismahane <arrisismahane@gmail.com>
2025-04-05 20:34:13 +0200

    add emperia api

---
+++ emperia/src/main/java/com/progdist/emperia/EmperiaApiApplication.java
@@ -0,0 +1,13 @@
+package com.progdist.emperia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmperiaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmperiaApiApplication.class, args);
+	}
+
+}
